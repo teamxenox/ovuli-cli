cc5c76b93bb6c5bb27e89e3b01e1df6b01983b87
shubhamarora15 <ashubham.1994@gmail.com>
2020-01-28 17:41:09 +0530

    ovilu feature added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
 const program = require("commander");
-
+// const ovuli = require('./util/ovuli');
+// console.log(ovuli.calculateOvuli('2020-01-01T11:12:25.338Z', 27))
 program.version("0.0.1");
